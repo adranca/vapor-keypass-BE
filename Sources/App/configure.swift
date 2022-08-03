e39e16f4970bf93cb3812957c8f2ea61e7184277
Alexandru Dranca <adranca@specialized.com>
2022-08-03 15:31:55 +0300

    asd

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,9 +1,16 @@
 import Vapor
+import NIOSSL
 
 // configures your application
 public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
-    // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+    try app.http.server.configuration.tlsConfiguration = .makeServerConfiguration(
+        certificateChain: NIOSSLCertificate.fromPEMFile("/Users/alexandru.dranca/.localhost-ssl/localhost.crt").map { .certificate($0)
+        },
+        privateKey: .privateKey(.init(file: "/Users/alexandru.dranca/.localhost-ssl/localhost.key", format: .pem))
+    )
+    
+     app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
     // register routes
     try routes(app)
